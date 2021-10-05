8a6d58fb89439f3a84170a06efd03b3602f4bdef
USERSAD\fgonzval <fco.gonza1997@gmail.com>
2021-10-05 17:15:21 -0300

    se agrego componentes a paginas module

--- src/app/compartida/compartida.module.ts
+++ src/app/compartida/compartida.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
+  exports: [
+    FooterComponent,
+    HeaderComponent
+  ],
+
   imports: [
     CommonModule
   ]
