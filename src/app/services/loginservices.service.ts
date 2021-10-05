b398aee160cf9bc6912ae2eb30baf3bfa035c37d
USERSAD\fgonzval <fco.gonza1997@gmail.com>
2021-10-05 17:02:38 -0300

    se agrego interfaces y services

--- src/app/services/loginservices.service.ts
+++ src/app/services/loginservices.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Login } from '../interfaces/login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginservicesService {
 
-  constructor() { }
+  login: Login[] = [];
+  // carrito:string[] = [];
+  constructor(private http: HttpClient) {
+      this.obtenerCuentasUsuarios();
+  }
+
+  private obtenerCuentasUsuarios() {
+      this.http.get('../assets/data/productoHombre.json').subscribe((resp: any) => {
+          this.login = resp.login;
+          
+          
+          
+      });
+  }
 }
