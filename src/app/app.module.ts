eae188577488a7d50889a858e4a03afb277577a7
USERSAD\fgonzval <fco.gonza1997@gmail.com>
2021-10-05 17:51:41 -0300

    rutas paginas actualizadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './compartida/header/header.component';
 import { LoginComponent } from './paginas/login/login.component';
 import { GeneralusuariosComponent } from './paginas/generalusuarios/generalusuarios.component';
 import { GeneralcursosComponent } from './paginas/generalcursos/generalcursos.component';
+import { CompartidaModule } from './compartida/compartida.module';
+import { PaginasModule } from './paginas/paginas.module';
 // import { RegistroComponent } from './login/registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    LoginComponent,
-    GeneralusuariosComponent,
-    GeneralcursosComponent,
+    // HeaderComponent,
+    // LoginComponent,
+    // GeneralusuariosComponent,
+    // GeneralcursosComponent,
     // RegistroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    CompartidaModule,
+    PaginasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
